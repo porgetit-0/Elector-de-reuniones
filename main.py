4e3d419e50c9b6d2e44089f91184ab0b7106cfa3
porgetit-0 <keesguerravin@gmail.com>
2021-03-02 13:17:06 -0500

    Programa básico de elección pseudo-aleatoria

---
+++ main.py
@@ -0,0 +1,10 @@
+from random import randint
+
+names = ["Meet","Alejandro","Jennyfer","Kevin"]
+meetings = []
+
+for i in range(0, 9):
+    number = randint(0,len(names)-1)
+    meetings.append(names[number])
+
+print(meetings)
